07cd0292b1de72c4146c2284b10448a688800a14
rpizarrog <rpizarro@itdurango.edu.mx>
2021-05-22 06:25:35 -0500

    Add files via upload

---
+++ scripts/cargar datos pizzas ventas aerolineas women.r
@@ -0,0 +1,6 @@
+# Cargar datos pizzas, ventas, aerolineas, women
+
+pizzas <- read.csv("https://raw.githubusercontent.com/rpizarrog/diplomado-cd-iot-2021/main/datos/pizzas.csv")
+ventas <- read.csv("https://raw.githubusercontent.com/rpizarrog/diplomado-cd-iot-2021/main/datos/ventas.csv")
+aerolineas <- read.csv("https://raw.githubusercontent.com/rpizarrog/diplomado-cd-iot-2021/main/datos/aerolineas.csv")
+women <- women
